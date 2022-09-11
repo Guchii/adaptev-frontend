389f49bea3f74518808e9be5c23c7addcf911f91
Shivom <shvomsrivastava@gmail.com>
2022-09-11 09:22:27 +0530

    add dynamic data links

--- pages/data.tsx
+++ pages/data.tsx
@@ -1,23 +1,35 @@
 import { NextPageContext } from "next";
 import type { FC } from "react";
+import { readdirSync } from "fs";
+import path from "path";
 
 export async function getStaticProps(context: NextPageContext) {
   const paths: Array<string> = [];
+  var files = readdirSync(path.join("public/data"));
+
+  paths.push(...files);
+
   return {
     props: {
       paths,
-    }, // will be passed to the page component as props
+    },
   };
 }
 
 const Data: FC<{ paths: string[] }> = ({ paths }) => {
   return (
     <section className="p-8">
-      <h1 className="text-4xl">Available Data:</h1>
+      <h1 className="pb-4 text-4xl">Available Data:</h1>
       {paths.map((path, i) => (
-        <span className="block font-bold" key={i}>
+        <a
+          className="block text-2xl font-semibold underline"
+          key={i}
+          href={"/data/" + path}
+          rel="noreferrer"
+          target={"_blank"}
+        >
           {path}
-        </span>
+        </a>
       ))}
     </section>
   );
